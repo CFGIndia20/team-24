72b64ae7e013de1db64e186dd714125f0dc6361d
VishalMakwana01 <vishal.makwana3@gmail.com>
2020-07-25 16:15:06 +0530

    Login and Register UI

--- web/client/thenudge/src/App.js
+++ web/client/thenudge/src/App.js
@@ -4,12 +4,14 @@ import Login from "./components/Auth/Login.js"
 import "bootstrap/dist/css/bootstrap.css"
 import HeaderComponent from "./components/Layout/HeaderComponent"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import RegisterComponent from './components/Auth/RegisterComponent';
 function App() {
   return (
     <Router>
       <HeaderComponent />
       <Switch>
         <Route path="/login" component={Login}></Route>
+        <Route path="/register" component={RegisterComponent}></Route>
       </Switch>
     </Router>
   );
