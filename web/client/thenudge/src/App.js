e00b03275b303fb396652e532ad53335781e7eb8
samakshimalhotra <samakshimalhotra24@gmail.com>
2020-07-26 02:44:32 +0530

    adding Teacherchedule

--- web/client/thenudge/src/App.js
+++ web/client/thenudge/src/App.js
@@ -20,23 +20,63 @@ import StudentRoute from './components/Common/StudentRoute';
 import AdminRoute from "./components/Common/AdminRoute";
 import TeacherRoute from "./components/Common/TeacherRoute";
 import AddJob from './components/Admin/addjob';
-
+import TeacherSchedule from './schedule';
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Switch>
-          <Route exact path="/" component={()=>{return(<><HeaderComponent/><Landing/></>)}} />
-          <Route exact path="/login" component={()=>{return(<><HeaderComponent/><Login/></>)}} />
-          <Route exact path="/register" component={()=>{return(<><HeaderComponent/><RegisterComponent/></>)}} />
+          <Route
+            exact
+            path="/"
+            component={() => {
+              return (
+                <>
+                  <HeaderComponent />
+                  <Landing />
+                </>
+              );
+            }}
+          />
+          <Route
+            exact
+            path="/login"
+            component={() => {
+              return (
+                <>
+                  <HeaderComponent />
+                  <Login />
+                </>
+              );
+            }}
+          />
+          <Route
+            exact
+            path="/register"
+            component={() => {
+              return (
+                <>
+                  <HeaderComponent />
+                  <RegisterComponent />
+                </>
+              );
+            }}
+          />
           <StudentRoute exact path="/student" component={StudentDashboard} />
           <StudentRoute exact path="/leaderboard" component={LeaderBoard} />
           <StudentRoute exact path="/jobs" component={JobListing} />
           <StudentRoute exact path="/schedule" component={Schedule} />
           <StudentRoute exact path="/userprofile" component={UserProfile} />
-          <TeacherRoute exact path="/teacherprofile" component={TeacherProfile} />
+          <TeacherRoute
+            exact
+            path="/teacherprofile"
+            component={TeacherProfile}
+          />
           <AdminRoute path="/adminprofile" component={AddJob} />
+          <AdminRoute path="/adminprofile" component={StudentDashboard} />
+         
         </Switch>
+        <Route exact path="/teacherdash" component={TeacherSchedule} />
       </Router>
     </Provider>
   );
---
+++ web/client/thenudge/src/schedule.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+// import SidebarComponent from "component/Layout/SidebarComponent";
+import "./components/Teacher/index.css";
+class Schedule extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        {/* <SidebarComponent /> */}
+        <div className="main">
+          <div className="container main-content">
+            <div class="card flex-row flex-wrap dash-cont bg-light">
+              <div class="card-header border-0">
+                <img src="//placehold.it/200" alt="" />
+              </div>
+              <div class="card-block px-2">
+                <h4 class="card-title mt-3">Cousre</h4>
+                <p class="card-text ">Description</p>
+                <a href="#" class="btn btn-primary">
+                  BUTTON
+                </a>
+              </div>
+              <div class="w-100"></div>
+              {/* <div class="card-footer w-100 text-muted">FOOTER</div> */}
+            </div>
+
+            <div class="card flex-row flex-wrap dash-cont bg-light">
+              <div class="card-header border-0 ">
+                <img src="//placehold.it/200" alt="" />
+              </div>
+              <div class="card-block px-2">
+                <h4 class="card-title mt-3">Title</h4>
+                <p class="card-text">Description</p>
+                <a href="#" class="btn btn-primary">
+                  BUTTON
+                </a>
+              </div>
+              <div class="w-100"></div>
+              {/* <div class="card-footer w-100 text-muted">FOOTER</div> */}
+            </div>
+
+            <div class="card flex-row flex-wrap dash-cont bg-light">
+              <div class="card-header border-0 ">
+                <img src="//placehold.it/200" alt="" />
+              </div>
+              <div class="card-block px-2">
+                <h4 class="card-title mt-3">Title</h4>
+                <p class="card-text">Description</p>
+                <a href="#" class="btn btn-primary">
+                  BUTTON
+                </a>
+              </div>
+              <div class="w-100"></div>
+              {/* <div class="card-footer w-100 text-muted">FOOTER</div> */}
+            </div>
+
+            <div class="card flex-row flex-wrap dash-cont bg-light">
+              <div class="card-header border-0 ">
+                <img src="//placehold.it/200" alt="" />
+              </div>
+              <div class="card-block px-2">
+                <h4 class="card-title mt-3">Title</h4>
+                <p class="card-text">Description</p>
+                <a href="#" class="btn btn-primary">
+                  BUTTON
+                </a>
+              </div>
+              <div class="w-100"></div>
+              {/* <div class="card-footer w-100 text-muted">FOOTER</div> */}
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Schedule;
