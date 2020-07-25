30dea18189ceafe6d901185c8c0df989df595619
jaY-S-99 <33809993+jaY-S-99@users.noreply.github.com>
2020-07-25 20:41:42 +0530

    Removed head bugs

--- web/client/thenudge/src/components/Auth/Login.js
+++ web/client/thenudge/src/components/Auth/Login.js
@@ -5,8 +5,8 @@ import { loginUser } from '../../redux/actions/authActions';
 import TextFieldGroup from '../Common/TextFieldGroupComponent';
 import SelectFieldGroup from "../Common/SelectFieldGroupComponent"
 import CardComponent from "../Common/CardComponent"
-import HeaderComponent from "../Layout/HeaderComponent"
 import "./login.css"
+
 class Login extends Component {
 
     constructor(props) {
@@ -42,7 +42,7 @@ class Login extends Component {
         const { errors } = this.state;
 
         return (
-<<<<<<< HEAD
+
           <div className="container mt-5 text-cream bg-main">
             <h1 className="display-4 mt-5 text-center">Login</h1>
             <div className="row justify-content-center">
@@ -82,55 +82,11 @@ class Login extends Component {
                     className="btn-primary  p-1 btn btn-lg btn-block mb-5"
                   >
                     Login
-=======
-            <>
-                <HeaderComponent />
-                <div className="container mt-5 text-cream bg-main">
-                    <h1 className="display-4 mt-5 text-center">Login</h1>
-                    <div className="row justify-content-center">
-                        <div className="col-md-4 col-lg-4 mt-5">
-                            <form
-                                className="form-container"
-                                onSubmit={this.onSubmit}
-                                noValidate
-                            >
-                                <TextFieldGroup
-                                    name="email"
-                                    type="email"
-                                    error={errors.email}
-                                    onChange={this.onChange}
-                                    placeholder="Email"
-                                    value={this.state.email}
-                                />
-                                <TextFieldGroup
-                                    name="password"
-                                    type="password"
-                                    error={errors.password}
-                                    onChange={this.onChange}
-                                    placeholder="Password"
-                                    value={this.state.password}
-                                />
-                                <SelectFieldGroup
-                                    name="designation"
-                                    type="text"
-                                    error={errors.designation}
-                                    onChange={this.onChange}
-                                    placeholder="Designation"
-                                    value={this.state.designation}
-                                    options={this.state.options}
-                                />
-                                <button
-                                    type="submit"
-                                    className="btn-primary  p-1 btn btn-lg btn-block mb-5"
-                                >
-                                    Login
->>>>>>> c1e6856bd6822ae9df0d5a1e6909bf13077f9e01
                   </button>
                             </form>
                         </div>
                     </div>
                 </div>
-            </>
         );
     }
 }
