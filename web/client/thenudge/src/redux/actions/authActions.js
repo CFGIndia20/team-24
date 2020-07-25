3ed818ea92723bd6270f3760cc6470d0cf1d06d2
jaY-S-99 <33809993+jaY-S-99@users.noreply.github.com>
2020-07-25 18:22:30 +0530

    User login actions added

--- web/client/thenudge/src/redux/actions/authActions.js
+++ web/client/thenudge/src/redux/actions/authActions.js
@@ -1,3 +1,41 @@
 import { GET_ERRORS, SET_USER_DATA } from "./types";
 import axios from "axios";
-import setAuthToken from "../../utils/setAuthToken";
+
+
+// Action for logging user in
+export const loginUser = (userData,userType) => (dispath) => {
+    if (userType == "admin"){
+        axios
+          .post("http://localhost:5000/adminlogin", {
+            Email: userData.email,
+            Password: userData.password,
+          })
+          .then((res) => {})
+          .catch((err) => {
+
+          });
+    }
+    if (userData.type == "student") {
+        axios
+          .post("http://localhost:5000/studentlogin", {
+            Email: userData.email,
+            Password: userData.password,
+          })
+          .then((res) => {})
+          .catch((err) => {
+
+          });
+
+    }
+    if (userData.type == "teacher") {
+
+        axios
+          .post("http://localhost:5000/teacherlogin", {
+            Email: userData.email,
+            Password: userData.password,
+          })
+          .then((res) => {})
+          .catch((err) => {});
+
+    }
+}
