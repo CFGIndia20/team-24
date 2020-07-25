9be12fc6d6971d1eda1e677bfdb20eac7e2840df
shahkshitij15 <shahkshitij15@gmail.com>
2020-07-25 16:57:24 +0530

    Algo 1.1

--- web/algo.py
+++ web/algo.py
@@ -1,25 +1,46 @@
 batches = {
-    "10": [1,2,3,4],
-    "20": [5,6,7,8]
+    "10": ['a','b','c','d'],
+    "20": ['e','f','g','h']
 }
 
 slot_preference = {
-    "1":[1,2,3],
-    "2":[4,5,6],
-    "3":[1,2,3],
-    "4":[2,4,5],
-    "5":[2,7,8],
-    "6":[3,8,9],
-    "7":[2,6,3],
-    "8":[1,5,6]
+    'a':[1,2,3],
+    'b':[4,5,6],
+    'c':[1,2,3],
+    'd':[2,4,5],
+    'e':[2,7,8],
+    'f':[3,8,7],
+    'g':[2,6,3],
+    'h':[1,5,6]
 }
 
+slot = {
+    1:[],
+    2:[],
+    3:[],
+    4:[],
+    5:[],
+    6:[],
+    7:[],
+    8:[]
+}
+
+def isValid(i):
+    if len(slot[i]) <= 15:
+        return True
+
+
 def check_preference():
+
     for key,value in batches.items():
         student = value
         for j in student:
             for i in slot_preference[j]:
-                print(i)
+                if isValid(i):
+                    slot[i].append(j)
+                    break
+    print(slot)
+                    
 
 
 
