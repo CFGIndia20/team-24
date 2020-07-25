c580b316b11289bd31a17f60dcd867cf445f0841
jaY-S-99 <33809993+jaY-S-99@users.noreply.github.com>
2020-07-25 15:27:46 +0530

    Added bootstrap and other dependencies

---
+++ web/client/thenudge/src/components/Layout/HeaderComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <div className="navbar navbar-expand-md navbar-light bg-light">
+                
+            </div>
+        )
+    }
+}
